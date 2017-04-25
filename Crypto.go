a102b32f971dd6c0e696e9a11d977a5aba13790a
codemaru <contact@codemaru.com>
2017-04-25 13:57:16 +0900

    gzipString 추가, Crypto 변경

--- Crypto.go
+++ Crypto.go
@@ -1,8 +1,29 @@
 package codemaru
 
 import (
+	"bytes"
+	"compress/gzip"
 	"encoding/base64"
 )
+
+func gzipStringPack(text []byte) []byte {
+	var b bytes.Buffer
+	w := gzip.NewWriter(&b)
+	w.Write(text)
+	w.Close()
+	return b.Bytes()
+}
+
+func gzipStringUnpack(text []byte) []byte {
+	var b bytes.Buffer
+	r, err := gzip.NewReader(&b)
+	if err != nil {
+		panic(err.Error())
+	}
+	r.Read(text)
+	r.Close()
+	return b.Bytes()
+}
 
 func ReverseString(s string) string {
 	runes := []rune(s)
@@ -16,10 +37,12 @@ func Encrypt(text []byte) []byte {
 	a := base64.StdEncoding.EncodeToString(text)
 	a = base64.StdEncoding.EncodeToString([]byte(ReverseString(a)))
 
-	return []byte(a)
+	return gzipStringPack([]byte(a))
 }
 
 func Decrypt(text []byte) []byte {
+	text = gzipStringUnpack(text)
+
 	a, err := base64.StdEncoding.DecodeString(string(text))
 	if err != nil {
 		panic(err.Error())
@@ -28,6 +51,5 @@ func Decrypt(text []byte) []byte {
 	if err != nil {
 		panic(err.Error())
 	}
-
 	return []byte(a)
 }
